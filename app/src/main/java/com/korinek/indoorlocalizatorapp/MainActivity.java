a84b4c823475028b2d9ca89fbdab51debe4a7fe5
Lukáš Kořínek <lukas.korinek@tul.cz>
2025-01-18 23:23:15 +0100

    Custom collapsing app toolbar implemented

--- app/src/main/java/com/korinek/indoorlocalizatorapp/MainActivity.java
+++ app/src/main/java/com/korinek/indoorlocalizatorapp/MainActivity.java
@@ -36,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_localization, R.id.navigation_building, R.id.navigation_settings)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
