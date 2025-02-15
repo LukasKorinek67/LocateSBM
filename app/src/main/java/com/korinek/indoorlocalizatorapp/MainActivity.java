2d3fa306287bf9e15888ffe825f48bfeb221db7c
Lukáš Kořínek <lukas.korinek@tul.cz>
2025-02-15 12:20:28 +0100

    Down padding fix, fragment navigation fix

--- app/src/main/java/com/korinek/indoorlocalizatorapp/MainActivity.java
+++ app/src/main/java/com/korinek/indoorlocalizatorapp/MainActivity.java
@@ -7,8 +7,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import com.korinek.indoorlocalizatorapp.databinding.ActivityMainBinding;
@@ -33,14 +32,10 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_localization, R.id.navigation_building, R.id.navigation_settings)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main);
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNav = findViewById(R.id.nav_view);
+        NavigationUI.setupWithNavController(bottomNav, navController);
     }
 
 }
